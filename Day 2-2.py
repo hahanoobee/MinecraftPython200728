0932073ddc7f99a4982a4bff9bba5a1cba462643
hahanoobee <68846035+hahanoobee@users.noreply.github.com>
2020-07-28 17:04:38 +0800

    Add files via upload

---
+++ Day 2-2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 10:21:50 2020
+
+@author: user
+"""
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+import random
+x,y,z=mc.player.getTilePos()
+
+color=random.randrange(0,16)
+mc.setBlocks(x+25,y-1,z+25,x-25,y-1,z-25,95,color)
